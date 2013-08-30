0e644e2005f63867df749f00413f10489eb676ef
Rob Vella <me@robvella.com>
2013-08-29 19:34:29 -0700

    Removing original tsp.js

--- test/tspsolver_test.js
+++ test/tspsolver_test.js
@@ -28,9 +28,27 @@ exports['tspsolver'] = {
     done();
   },
   'no args': function(test) {
-    test.expect(1);
+    test.expect(4);
     // tests here
-    test.equal(tspsolver.awesome(), 'awesome', 'should be awesome.');
-    test.done();
+
+    tspsolver.addAddress("100 WILSHIRE BLVD, SANTA MONICA, CA");
+    tspsolver.addAddress("1 WILSHIRE BLVD, SANTA MONICA, CA");
+    tspsolver.addAddress("7000 Hollywood Blvd, Los Angeles, CA");
+
+    tspsolver.solveRoundTrip(function() {
+        var addresses = tspsolver.getAddresses();
+
+        console.log(addresses);
+        test.equal(addresses[0], '100 WILSHIRE BLVD, SANTA MONICA, CA', 'should be 100 WILSHIRE BLVD, SANTA MONICA, CA');
+        test.equal(addresses[1], '1 WILSHIRE BLVD, SANTA MONICA, CA', 'should be 1 WILSHIRE BLVD, SANTA MONICA, CA');
+        test.equal(addresses[2], '7000 Hollywood Blvd, Los Angeles, CA', 'should be 7000 Hollywood Blvd, Los Angeles, CA');
+
+        var link = tspsolver.createGoogleLink();
+        var expected_link = "http://maps.google.com/maps?saddr=100%20WILSHIRE%20BLVD%2C%20SANTA%20MONICA%2C%20CA&daddr=7000%20Hollywood%20Blvd%2C%20Los%20Angeles%2C%20CA to:1%20WILSHIRE%20BLVD%2C%20SANTA%20MONICA%2C%20CA to:100%20WILSHIRE%20BLVD%2C%20SANTA%20MONICA%2C%20CA";
+
+        test.equal(link,expected_link, "should be "+expected_link);
+
+        test.done();
+    });
   }
 };
